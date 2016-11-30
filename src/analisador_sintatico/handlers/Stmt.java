b93958d8c4dee7cacd7c7c3c16b6d12015b75f53
Talles Alves <tallesventura@yahoo.com.br>
2016-11-30 11:16:33 -0200

    ifs de Stmt trocados para switch

--- src/analisador_sintatico/handlers/Stmt.java
+++ src/analisador_sintatico/handlers/Stmt.java
@@ -23,6 +23,7 @@ public class Stmt extends AbstractHandler {
     @Override
     public boolean handle() {
         if (nextToken()) {
+            /*
             if (currentToken == Token.IDENTIFICADOR || currentToken == Token.DELETE || 
                     currentToken == Token.PASS || currentToken == Token.BREAK || 
                     currentToken == Token.CONTINUE || currentToken == Token.RETURN || 
@@ -39,9 +40,38 @@ public class Stmt extends AbstractHandler {
                 errorCode = 3;
                 return false;
             }
+            */
+            int flag = switchHelper();
+            switch(flag){
+                case 1:
+                    return new Simple_stmt(tokens).handle();
+                case 2:
+                    return new Compound_stmt(tokens).handle();
+                default:
+                    errorCode = 3;
+                    return false;
+            }
         } else {
             errorCode = 25;
             return false;
         }
+    }
+    
+    private int switchHelper(){
+        
+        if(currentToken == Token.IDENTIFICADOR || currentToken == Token.DELETE || 
+                    currentToken == Token.PASS || currentToken == Token.BREAK || 
+                    currentToken == Token.CONTINUE || currentToken == Token.RETURN || 
+                    currentToken == Token.YIELD || currentToken == Token.IMPORT || 
+                    currentToken == Token.GLOBAL || currentToken == Token.NONLOCAL){
+            return 1;
+        }else if(currentToken == Token.IF || currentToken == Token.WHILE || 
+                    currentToken == Token.DEF || currentToken == Token.CLASS || 
+                    currentToken == Token.OP_ARROBA || currentToken == Token.ASYNC){
+            return 2;
+        }else{
+            return -1;
+        }
+        
     }
 }
